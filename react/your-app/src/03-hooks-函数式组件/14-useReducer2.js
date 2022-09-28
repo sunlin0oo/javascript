dbacbfafb679ff6fa62eec7d820c2466174c5517
sunlinMain <sunzifeng@simverse.cn>
2022-09-28 23:23:49 +0800

    复习函数式组件

--- react/your-app/src/03-hooks-函数式组件/14-useReducer2.js
+++ react/your-app/src/03-hooks-函数式组件/14-useReducer2.js
@@ -5,6 +5,7 @@ const initailState = {
 }
 
 const reducer = (prevState,action)=>{
+    // prevState接收的上一次的状态值,action接收的是dispatch传来的值
     let newstate = {...prevState}
     switch(action.type){
         case "change-a":
@@ -25,9 +26,10 @@ export default function App() {
     // 唯一的数据源
     const [state, dispatch] = useReducer(reducer, initailState)
     return (
+        // 供应商提供方法和参数(value)
         <GlobalContext.Provider value={
             {
-                state,
+                state,// 这里的state是initailState
                 dispatch
             }
         }>
@@ -42,15 +44,18 @@ export default function App() {
 }
 
 function Child1(){
-    const {dispatch} = useContext(GlobalContext)//这里拿到的值是整个GlobalContext中value对象
+    // 直接将GlobalContext当参数传入==>这里value返回的是供应商所提供的服务对象(value),无需下面进行回调
+    const {dispatch} = useContext(GlobalContext)//去解构找出其中的dispatch方法
     return(
         <div style={{background:"red"}}>
+            {/* 当点击函数触发 dispatch 将 值传给reducer===>state改变==>重新渲染所有组件 */}
             <button onClick={()=>{
                 dispatch({
                     type:"change-a",
                     value:"aaaaaaaa"
                 })
             }}>改变a</button>
+
             <button onClick={()=>{
                 dispatch({
                     type:"change-b",
@@ -64,13 +69,15 @@ function Child1(){
 function Child2(){
     const {state} = useContext(GlobalContext)//这里拿到的值是整个GlobalContext中value对象
     return (
-        <div style={{background:"yellow"}}>child2-{state.a}</div>
+       
+        
+        <div style={{background:"yellow"}}>child2-{state.a}-{console.log('a')}</div>
     )
 }
 
 function Child3(){
     const {state} = useContext(GlobalContext)//这里拿到的值是整个GlobalContext中value对象
     return (
-        <div style={{background:"green"}}>child3-{state.b}</div>
+        <div style={{background:"green"}}>child3-{state.b}-{console.log('b')}</div>
     )
 }
