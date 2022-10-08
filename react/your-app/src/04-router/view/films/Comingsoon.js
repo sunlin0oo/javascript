bb27fee8855227f7b53ac5397abfd9ab15064cd5
sunzifeng <sunzifeng@simverse.cn>
2022-10-08 16:42:28 +0800

    反向代理

--- react/your-app/src/04-router/view/films/Comingsoon.js
+++ react/your-app/src/04-router/view/films/Comingsoon.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class Comingsoon extends Component {
-  // 反向代理
+  // 反向代理==>跨域问题是浏览器阻止，可以通过http://localhost:3000与猫眼进行数据通信，然后再往自己服务器进行数据请求
+//   成功render并渲染完成真实DOM之后触发，可以修改DOM==>适合做axios请求
     componentDidMount() {
         axios.get("/ajax/comingList?ci=1&limit=10&movieIds=&token=&optimus_uuid=0213411066DB11EC97E5B17F51643A714D3DCE91F9A042A0B8038AF6545D2CFB&optimus_risk_level=71&optimus_code=10").then(res=>{
             console.log("res.data::",res.data)
--- react/your-app/src/setupProxy.js
+++ react/your-app/src/setupProxy.js
@@ -2,17 +2,21 @@
  * @作者: kerwin
  * @公众号: 大前端私房菜
  */
-// 这一块是啥来 我忘了 如果复习到重点看一看
+// 核心跨域中间件==>能够进行反向代理
 const { createProxyMiddleware } = require('http-proxy-middleware');
-// 每次修改完需要重启服务器
+// !!!每次修改完setupProxy需要重启服务器!!!!
 module.exports = function(app) {
   app.use(
-    '/ajax',  // /api/list /api/detail /api/center /api/cart  请求路径
+    '/ajax',  // /api/list /api/detail /api/center /api/cart  你所要请求路径==>是公共路径==>以ajax开头的就走到这个代理
     createProxyMiddleware({
-      target: 'https://i.maoyan.com',//目标;会加上请求路径，进行请求
+      target: 'https://i.maoyan.com',//目标;会加上请求路径(Comingsoon==>axios所请求的地址)，进行请求
       changeOrigin: true
     })
   );
+
+  
+//https://i.maoyan.com/
+//ajax/comingList?ci=1&limit=10&movieIds=&token=&optimus_uuid=B0CC71C096A911EBA609453370FBFE2C59BC3AF5D61A4344A1A7C05FABCF4D21&optimus_risk_level=71&optimus_code=10
 
   app.use(
     '/graphql',
