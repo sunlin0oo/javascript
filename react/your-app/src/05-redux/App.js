afaf4ffa3ff102c26d56171d0082977f5bcd6288
sunzifeng <sunzifeng@simverse.cn>
2022-10-08 17:15:34 +0800

    redux工作流

--- react/your-app/src/05-redux/App.js
+++ react/your-app/src/05-redux/App.js
@@ -3,7 +3,9 @@ import Tabbar from './components/Tabbar'
 import store from './redux/store'
 import MRouter from './router/IndexRouter'
 import './view/css/App.css'
-
+/**  Flex是一个管理框架思想，redux是思想的实现
+ * 工作流:组件状态要改动==>发送 action(createAction)==dispatch==>发送到store中，在store中通过reducer(新老状态比较)进行状态更新==>通知相关组件进行更新 
+*/
 export default class App extends Component {
   state = {
     // isShow:store.getState(),
