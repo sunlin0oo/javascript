2aa6b6c0a6e3b93c618aefee4fe060b46035fa10
sunlinMain <sunzifeng@simverse.cn>
2022-10-16 21:54:39 +0800

    动态路由

---
+++ react/app-Umi/src/pages/detail/[id].tsx
@@ -0,0 +1,14 @@
+// 这里[id]==>会动态取id
+import React from "react";
+import { useParams } from "umi";
+
+export default function Detail(props: any) {
+    // console.log(props.params.id);与useParams 相同
+    const params = useParams();
+    console.log(params.id);
+    return (
+        <div>
+            Detail
+        </div>
+    )
+}
