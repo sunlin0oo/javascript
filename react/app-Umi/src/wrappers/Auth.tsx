7d686af61b3048719d254790db4d96571243c105
sunlinMain <sunzifeng@simverse.cn>
2022-10-16 23:22:13 +0800

    校验组件==>如果没有通过则跳转到login中

--- react/app-Umi/src/wrappers/Auth.tsx
+++ react/app-Umi/src/wrappers/Auth.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect } from "umi";
 
 export default function Auth(props: any) {
+    // 校验组件==>如果没有通过则跳转到login中
     if (localStorage.getItem('token')) {
         return (
             <div>
