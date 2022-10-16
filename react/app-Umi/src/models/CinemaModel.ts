a5adb33bfbb7b325c225378c2adc333079c48a7f
sunlinMain <sunzifeng@simverse.cn>
2022-10-17 00:08:46 +0800

    实现Cinema页面与city页面的联动(更改城市变化电影院列表)

--- react/app-Umi/src/models/CinemaModel.ts
+++ react/app-Umi/src/models/CinemaModel.ts
@@ -1,13 +1,48 @@
-export default{
-    namespace:'cinema',
-    state:{
-        list:[]
+export default {
+    namespace: 'cinema',
+    state: {
+        list: []
+    },
+    reducers:{
+        clearList(prevState:any,action:any){
+            console.log('action', action, 'prevState', prevState)
+            return{
+                ...prevState,
+                list:[]
+            }
+        },
+        changeList(prevState:any,action:any){
+            console.log('action', action, 'prevState', prevState)
+            return{
+                ...prevState,
+                list:action.payload
+            }
+        }
     },
     // 副作用函数处理异步操作==>dva会去找reducer及effects是否有对应的函数
-    effects:{
+    effects: {
+        // 生成器函数
         // action是dispatch的数据;obj是对于saga的封装
-        *getList(action:any,obj:any){
-            // console.log('action,obj', action,obj)
+        *getList(action: any, obj: any):any {
+            console.log('action,obj', action,obj)
+            const { put, call } = obj;
+            // 等待异步的返回值
+            let res = yield call(getListForCinema,action.payload.cityId);
+            // 传给reducer
+            yield put({
+                type:'changeList',
+                payload:res
+            });
         }
     }
+}
+// 数据请求函数
+async function getListForCinema(cityId:String) {
+    let res = await fetch(`https://m.maizuo.com/gateway?cityId=${cityId}&ticketFlag=1&k=7406159`, {
+        headers: {
+            'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.0.4","e":"16395416565231270166529","bc":"110100"}',
+            'X-Host': 'mall.film-ticket.cinema.list'
+        }
+    }).then(res=>res.json())
+    return res.data.cinemas
 }
--- react/app-Umi/src/pages/Cinema.tsx
+++ react/app-Umi/src/pages/Cinema.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { NavBar } from 'antd-mobile'
 import { SearchOutline } from 'antd-mobile-icons'
+import { DotLoading } from 'antd-mobile'
 import { connect } from "umi";
 /**
  * // back={props.cityName}是要显示城市名称，所以要当到dva中进行管理
@@ -8,41 +9,58 @@ import { connect } from "umi";
  * 2.CityModel中修改组件状态
  * 3.connect()(Cinema)==>创建高阶组件==>可以监听到models中的状态
  */
-function Cinema(props:any) {
+function Cinema(props: any) {
     console.log('props', props);
-    useEffect(()=>{
-        if(props.list.length === 0){
-            // 取数据==>异步操作
+    useEffect(() => {
+        if (props.list.length === 0) {
+            // 取数据==>异步操作==?dispatch到CinemaModel
             props.dispatch({
-                type:'cinema/getList',
-                payload:{
-
+                type: 'cinema/getList',
+                payload: {
+                    cityId: props.cityId
                 }
             })
-        }else{
+        } else {
             console.log('缓存')
         }
-    },[])
+    }, [])
     return (
         <div>
             {/* 搜索栏进行城市的切换 */}
             <NavBar onBack={() => {
                 // console.log('Click')
+
+                // 在点击切换城市的时候清空cinema list的数据
+                props.dispatch({
+                    type: 'cinema/clearList'
+                })
                 props.history.push('/city')
             }} back={props.cityName} backArrow={false} right={<SearchOutline></SearchOutline>}>标题</NavBar>
-            Cinema
+            {
+                props.loading && <div style={{ fontSize: 14, textAlign: 'center' }}>
+                    <DotLoading color='red' />
+                </div>
+            }
+            <ul>
+                {
+                    props.list.map((item: any) => <li key={item.cinemaId}>{item.name} </li>)
+                }
+            </ul>
         </div>
     )
 }
 
 // connect 帮cinema创建父组件，订阅发布等功能==>只要被包装就可以获取到所有的命名空间
-export default connect((state:any)=>{
+export default connect((state: any) => {
     console.log(state)
     // 获取到models中的数据
-    return{
-        a:1,
+    return {
+        a: 1,
+        // dva中自带的加载属性==>异步流程未走完是false，加载完成是true
+        loading: state.loading.global,
         // state.(命名空间).(空间中的属性)
-        cityName:state.city.cityName,
-        list:state.cinema.list
+        cityName: state.city.cityName,
+        cityId: state.city.cityId,
+        list: state.cinema.list
     }
 })(Cinema)
