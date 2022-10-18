373788fe814b4acd706345c18977c834f5bf6c39
sunlinMain <sunzifeng@simverse.cn>
2022-10-18 22:52:08 +0800

    声明式导航的新写法

--- react/react-router6/src/App.js
+++ react/react-router6/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import MRouter from './router';
+import Tabbar from './components/Tabbar';
 function App() {
   return (
     <HashRouter>
       <MRouter></MRouter>
+      <Tabbar></Tabbar>
     </HashRouter>
   );
 }
