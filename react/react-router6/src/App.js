2fd3228f13c81f813d1f089e85c333588a4a444b
sunlinMain <sunzifeng@simverse.cn>
2022-10-20 00:05:40 +0800

    路由模式

--- react/react-router6/src/App.js
+++ react/react-router6/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import { BrowserRouter } from 'react-router-dom'; 存在问题:会向后端请求路由，如果没有对应路由会返回404 
 import { HashRouter, Route, Routes } from 'react-router-dom'
 import MRouter from './router';
 import Tabbar from './components/Tabbar';
