f8e4675117b605c1741a6ebd96c24d29714d70ae
sunzifeng <sunzifeng@simverse.cn>
2022-11-03 09:38:35 +0800

    备注

--- react/your-app/src/index.js
+++ react/your-app/src/index.js
@@ -9,8 +9,9 @@ import { createRoot } from 'react-dom/client';
 // 必须要首字母大写
 // 将div插入到id===root的节点中==>JSX
 
-const container = document.getElementById('root');
+const container = document.getElementById('root');// DOM根节点=>React.createElement() 会预先执行一些检查，以帮助你编写无错代码，但实际上它创建了一个这样的对象：
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// <App /> 是一段jsx
 root.render(
   // redux持久化
     <App/>
