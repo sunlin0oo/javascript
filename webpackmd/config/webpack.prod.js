094f2013f57d14636d5eba49836462a5aa18a126
sunlinMain <sunzifeng@simverse.cn>
2023-09-14 00:31:16 +0800

    add: 单独提取CSS文件及样式兼容操作

--- webpackmd/config/webpack.prod.js
+++ webpackmd/config/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path') // 导入node.js 中专门操作路径模块
 const ESLintPlugin = require('eslint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 //使用node.js 中的导出语法，向外导出 一个 webpack配置对象
 module.exports = {
@@ -29,16 +30,47 @@ module.exports = {
         test: /\.css$/i,  //只检测.css文件
         // 执行顺序，从右往左(从下到上)
         use: [
-          "style-loader", // 将js中css通过创建style标签添加到html文件中生效
-          "css-loader"  // 将css资源编译commonjs的模块js中
+          MiniCssExtractPlugin.loader, // 提取CSS为单独的文件
+          // "style-loader", // 将js中css通过创建style标签添加到html文件中生效
+          "css-loader",  // 将css资源编译commonjs的模块js中
+          // 给loader写配置项需要写成对象形式
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-preset-env', // 可以解决大多数样式兼容性问题
+                ]
+              }
+            }
+          }
         ],
       },
       {
         test: /\.less$/i,
         use: [
           // compiles Less to CSS
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-preset-env', // 可以解决大多数样式兼容性问题
+                ]
+              }
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-preset-env', // 可以解决大多数样式兼容性问题
+                ]
+              }
+            }
+          },
           'less-loader',// 将less编译成css
         ],
       },
@@ -46,9 +78,19 @@ module.exports = {
         test: /\.s[ac]ss$/i,
         use: [
           // 将 JS 字符串生成为 style 节点
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           // 将 CSS 转化成 CommonJS 模块
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  'postcss-preset-env', // 可以解决大多数样式兼容性问题
+                ]
+              }
+            }
+          },
           // 将 Sass 编译成 CSS
           'sass-loader',
         ],
@@ -98,7 +140,10 @@ module.exports = {
       template: path.join(__dirname, '../public/index.html'),
       filename: 'webpack.html', // 生成的html文件的名字
       inject: 'body' // webpack打包生成的js文件插入的位置（默认插入head标签）
-    })
+    }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/main.css"
+    }),
   ],
   // mode: 'development', // 开发模式
   mode: "production" // 生产环境
